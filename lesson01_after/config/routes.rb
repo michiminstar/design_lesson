0d2c54dd021354273a32e18718a320cd25f05197
Michi Kanda <michi.kanda.0513@gmail.com>
2014-12-09 23:22:35 +0900

    Change application.js

--- lesson01_after/config/routes.rb
+++ lesson01_after/config/routes.rb
@@ -2,7 +2,6 @@ Lesson01::Application.routes.draw do
   get "landing/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
